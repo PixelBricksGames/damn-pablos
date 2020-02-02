3822eadb9c146ae4f786ffdfea19f2b19aed76d0
Pabrick <pabrick.bastaky@gmail.com>
2020-02-02 17:18:51 +0000

    :rabbit: Add @pabrick/roger library

--- src/app/components/root/Root.js
+++ src/app/components/root/Root.js
@@ -4,6 +4,11 @@ import "./Root.scss";
 import InfoContainer from "../info/Info.container";
 import LaboratoryContainer from "../laboratory/Laboratory.container";
 import MenuContainer from "../menu/Menu.container";
+import Roger from "@pabrick/roger";
+
+const rClock = new Roger.Clock(0.1);
+rClock.setDebugMode(true);
+rClock.start();
 
 const Root = () => (
     <main className="app">
