62e897130607bf33252fe54813773c9443e2edd7
Pabrick <pabrick.bastaky@gmail.com>
2020-02-12 08:09:22 +0000

    🚧  Unlock functionality

--- src/app/store/state/game.init.js
+++ src/app/store/state/game.init.js
@@ -1,4 +1,7 @@
 export const game = {
+	clonesPerSecond: 0,
+	clonesPerClick: 1,
+	outbreak: false,
 	currency: {
 		clones: 0,
 		money: 0,
@@ -10,7 +13,38 @@ export const game = {
 		rebellion: 0,
 		mutation: 0
 	},
-	clonesPerSecond: 0,
-	clonesPerClick: 1,
-	outbreak: false
+	unlock: {
+		tools: {
+			autoClone: false,
+			autoSell: false,
+			autoSerum: false,
+		},
+		clones: {
+			child: {
+				clone: false,
+				sell: false,
+				serum: false
+			},
+			teen: {
+				clone: false,
+				sell: false,
+				serum: false
+			},
+			adult: {
+				clone: false,
+				sell: false,
+				serum: false
+			},
+			senior: {
+				clone: false,
+				sell: false,
+				serum: false
+			},
+			ancient: {
+				clone: false,
+				sell: false,
+				serum: false
+			}
+		}
+	}
 };
