219add4bd409753132071aa3f6344c121887b159
Pabrick <pabrick.bastaky@gmail.com>
2020-02-16 06:49:05 +0000

    🚧 Add Interface state

---
+++ src/app/store/reducers/interface.reducer.js
@@ -0,0 +1,11 @@
+import * as actionType from "../../units/actions.type";
+import * as initState from "../state/interface.init";
+
+const interface = (state = initState.interface, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+export default interface;
---
+++ src/app/store/state/interface.init.js
@@ -0,0 +1,34 @@
+export const interface = {
+	open: false,
+	notifications: 0,
+	tab: {
+		clones: {
+			open: false,
+			notifications: 0
+		},
+		tools: : {
+			open: false,
+			notifications: 0
+		},
+		upgrades: : {
+			open: false,
+			notifications: 0
+		},
+		achievements: : {
+			open: false,
+			notifications: 0
+		},
+		stats: : {
+			open: false,
+			notifications: 0
+		},
+		config: : {
+			open: false,
+			notifications: 0
+		},
+		credits: : {
+			open: false,
+			notifications: 0
+		},
+	}
+};
