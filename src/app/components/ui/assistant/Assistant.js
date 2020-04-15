994979efdb3757ce135af337714504db12ad4f5e
Pabrick <pabrick.bastaky@gmail.com>
2020-04-15 22:56:19 +0100

    💫 Add fisrt asistant animations

--- src/app/components/ui/assistant/Assistant.js
+++ src/app/components/ui/assistant/Assistant.js
@@ -6,21 +6,27 @@ import { translations } from "../../../units/translations";
 import * as ANIMATIONS from "../../../units/animations";
 import "./Assistant.scss";
 
-const ssPablosHeads = new Roger.RegularSheet("./../../../assets/images/common/spritesheet.png", {w:512,h:512}, {w:54,h:90});
-const pabloIdleSprite = new Roger.Sprite("./../../../assets/images/common/spritesheet.png", {w:54,h:90}, {x:0,y:0});
-const animPablos_blink1 = new Roger.Animation("blink1",
-										ssPablosHeads,
-										[0,1,2,1,0],
-										{ delay: 10, loops: -1, callBack: ()=> {
-												console.log("blink complete!");
-											}
-										});
+const ssPablosHeads = new Roger.RegularSheet("./../../../assets/images/assistant/assistant-head-sheet.png", {w:480,h:525}, {w:96,h:105});
+const spritePabloHead_camera = new Roger.Sprite("./../../../assets/images/assistant/assistant-head-sheet.png", {w:480,h:525}, {x:0,y:0});
+const spritePabloMouth_idle = new Roger.Sprite("./../../../assets/images/assistant/assistant-head-sheet.png", {w:480,h:525}, {x:384,y:210});
 
-const animPablos_talk1 = new Roger.Animation("talk1",
-										ssPablosHeads,
-										[3,4,5,6,7],
-										{ direction: "random" });
+// const pabloHead_camera = new Roger.Animation("head-camera", ssPablosHeads, [0], { loops: -1 });
+// const pabloHead_right = new Roger.Animation("head-right", ssPablosHeads, [1], { loops: -1 });
+// const pabloHead_left = new Roger.Animation("head-left", ssPablosHeads, [2], { loops: -1 });
+// const pabloHead_up = new Roger.Animation("head-up", ssPablosHeads, [3], { loops: -1 });
+// const pabloHead_down = new Roger.Animation("head-down", ssPablosHeads, [4], { loops: -1 });
 
+const animPablo_blink = new Roger.Animation("blink", ssPablosHeads, [10,11,12, 13, 13, 11,10], { delay: 20, loops: -1 });
+const animPablo_talk1 = new Roger.Animation("talk1", ssPablosHeads, [15, 16, 17, 18, 19], { direction: "random" });
+
+// const loadHeadAnimations = (rogerToon) => {
+// 	// rogerToon.addAnimation(pabloHead_camera);
+// 	rogerToon.addAnimation(pabloHead_right);
+// 	rogerToon.addAnimation(pabloHead_left);
+// 	rogerToon.addAnimation(pabloHead_up);
+// 	rogerToon.addAnimation(pabloHead_down);
+// 	return rogerToon;
+// }
 
 class Assistant extends React.Component {
 	pabloToon;
@@ -40,16 +46,32 @@ class Assistant extends React.Component {
 	}
 
 	componentDidMount() {
-		this.pabloToon = new Roger.Toon("pablo", pabloIdleSprite);
-		this.pabloToon.addAnimation(animPablos_blink1);
-		this.pabloToon.addAnimation(animPablos_talk1);
-		rClock.addToonToUpdate(this.pabloToon);
+		this.pabloHead = new Roger.Toon("pablo-base", spritePabloHead_camera);
+		// this.pabloHead = loadHeadAnimations(this.pabloHead);
+		rClock.addToonToUpdate(this.pabloHead);
+
+		this.pabloEyelids = new Roger.Toon("pablo-eyelids");
+		this.pabloEyelids.addAnimation(animPablo_blink);
+		rClock.addToonToUpdate(this.pabloEyelids);
+
+		this.pabloMouth = new Roger.Toon("pablo-mouth", spritePabloMouth_idle);
+		this.pabloMouth.addAnimation(animPablo_talk1);
+		rClock.addToonToUpdate(this.pabloMouth);
+
+		// rClock.setDebugMode(true);
 	}
 
 	render() {
 		return (
 			<section className="assistant">
-				<div id="pablo" className="pablo"></div>
+				<div className="pablo">
+					<div className="pablo__head">
+						<div id="pablo-base"></div>
+						<div id="pablo-mouth"></div>
+						<div id="pablo-eyelids"></div>
+						<div id="pablo-eyebrows"></div>
+					</div>
+				</div>
 			</section>
 		);
 	}
@@ -60,13 +82,14 @@ class Assistant extends React.Component {
 			if(animations.face === ANIMATIONS.ASSISTANT.FACE.NORMAL
 			&& animations.face !== this.currentAnimations.face) {
 				this.currentAnimations.face = animations.face;
-				this.pabloToon.playAnimation("blink1");
+				this.pabloEyelids.playAnimation("blink");
+				this.pabloMouth.playAnimation("talk1");
 			}
 
 			if(animations.mouth === ANIMATIONS.ASSISTANT.MOUTH.TALK
 			&& animations.mouth !== this.currentAnimations.mouth) {
 				this.currentAnimations.mouth = animations.mouth;
-				this.pabloToon.playAnimation("talk1");
+				this.pabloMouth.playAnimation("talk1");
 			}
 
 		}
